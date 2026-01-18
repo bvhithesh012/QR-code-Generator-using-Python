de1f044c81ec5b4a38f4b3c1e39ac0461da7dfd5
bvhithesh012 <bvhithesh30@gmail.com>
2026-01-18 23:36:00 +0530

    Update qr code.py

--- qr code.py
+++ qr code.py
@@ -1,7 +1,7 @@
 import qrcode
 
 url = input("Enter the URL: ").strip()
-file_path = "("enter the file_path location")qrcode.png"
+file_path = "(#enter the file_path location)qrcode.png"
 
 qr=qrcode.QRCode()
 qr.add_data(url)
@@ -11,3 +11,4 @@ img.save(file_path)
 
 
 print("QR code was Generated!, saved to file_path !")
+
