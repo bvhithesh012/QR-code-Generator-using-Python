b0d70bd3940dfd945e5aa7d54de3310fb686af8b
bvhithesh012 <bvhithesh30@gmail.com>
2026-01-18 23:35:29 +0530

    Update qr code.py

--- qr code.py
+++ qr code.py
@@ -1,12 +1,13 @@
 import qrcode
 
 url = input("Enter the URL: ").strip()
-file_path = "C:\\Users\\Admin\\OneDrive\\Desktop\\qrcode.png"
+file_path = "("enter the file_path location")qrcode.png"
 
 qr=qrcode.QRCode()
 qr.add_data(url)
 
 img = qr.make_image()
 img.save(file_path)
+
 
 print("QR code was Generated!, saved to file_path !")
